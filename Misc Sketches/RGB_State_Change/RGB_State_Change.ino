6d71b82303830917b5e5e0f65146d0da65eec2d4
Josiah Pang <pang.josiah@gmail.com>
2017-04-29 18:31:11 -0700

    RGB state change works!!!

--- Misc Sketches/RGB_State_Change/RGB_State_Change.ino
+++ Misc Sketches/RGB_State_Change/RGB_State_Change.ino
@@ -1,35 +1,35 @@
 /*
   State change detection (edge detection)
 
- Often, you don't need to know the state of a digital input all the time,
- but you just need to know when the input changes from one state to another.
- For example, you want to know when a button goes from OFF to ON.  This is called
- state change detection, or edge detection.
+  Often, you don't need to know the state of a digital input all the time,
+  but you just need to know when the input changes from one state to another.
+  For example, you want to know when a button goes from OFF to ON.  This is called
+  state change detection, or edge detection.
 
- This example shows how to detect when a button or button changes from off to on
- and on to off.
+  This example shows how to detect when a button or button changes from off to on
+  and on to off.
 
- The circuit:
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- * LED attached from pin 13 to ground (or use the built-in LED on
+  The circuit:
+   pushbutton attached to pin 2 from +5V
+   10K resistor attached to pin 2 from ground
+   LED attached from pin 13 to ground (or use the built-in LED on
    most Arduino boards)
 
- created  27 Sep 2005
- modified 30 Aug 2011
- by Tom Igoe
+  created  27 Sep 2005
+  modified 30 Aug 2011
+  by Tom Igoe
 
-This example code is in the public domain.
+  This example code is in the public domain.
 
- http://www.arduino.cc/en/Tutorial/ButtonStateChange
+  http://www.arduino.cc/en/Tutorial/ButtonStateChange
 
- */
+*/
 
 // ==========
 // Variables
 // ==========
 // this constant won't change:
-const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+const int  buttonPin = 16;    // the pin that the pushbutton is attached to
 const int ledPin = 21;       // the pin that the LED is attached to
 // Declares RGB LED pins
 int redPin = 5;   // Red LED,   connected to digital pin 5
@@ -51,8 +51,8 @@ void setup() {
   pinMode(redPin, OUTPUT);   // sets the pins as output
   pinMode(grnPin, OUTPUT);
   pinMode(bluPin, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("----BEGIN----");
+  //Serial.begin(9600);
+  //Serial.println("----BEGIN----");
 }
 
 
@@ -66,14 +66,15 @@ void loop() {
     if (buttonState == HIGH) {
       // if the current state is HIGH then the button
       // wend from off to on:
-      buttonPushCounter++;
-      Serial.println("on");
-      Serial.print("number of button pushes:  ");
-      Serial.println(buttonPushCounter);
-    } else {
-      // if the current state is LOW then the button
-      // wend from on to off:
-      Serial.println("off");
+      if (buttonPushCounter == 5) {
+        buttonPushCounter = 0;
+      }
+      else {
+        buttonPushCounter++;
+      }
+      //Serial.println("on");
+      //Serial.print("number of button pushes:  ");
+      //Serial.println(buttonPushCounter);
     }
     // Delay a little bit to avoid bouncing
     delay(50);
@@ -87,10 +88,40 @@ void loop() {
   // checking the modulo of the button push counter.
   // the modulo function gives you the remainder of
   // the division of two numbers:
-  if (buttonPushCounter % 2 == 0) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+  if (buttonPushCounter == 0) {
+    //digitalWrite(ledPin, HIGH);
+    ledColor(255, 0, 0, 0.5); // Red
+  }
+  else if (buttonPushCounter == 1) {
+    ledColor(0, 255, 0, 0.5); // Green
+  }
+  else if (buttonPushCounter == 2) {
+    ledColor(0, 0, 255, 0.5); // Blue
+  }
+  else if (buttonPushCounter == 3) {
+    ledColor(255, 255, 255, 0.5); // White is right
+  }
+  else if (buttonPushCounter == 4) {
+    ledColor(100, 50, 100, 0.5); // ?
+  }
+  else if (buttonPushCounter == 5) {
+    ledColor(75, 223, 17, 0.5); // ??
+  }
+  else {
+    ledColor(0, 0, 0, 0); // Off
   }
 
+}
+
+// Function to write LED
+// brightness is a float between 0 to 1 representing brightness percentage
+void ledColor(int r, int g, int b, float brightness)
+{
+  int redVal = (r * brightness);
+  int grnVal = (g * brightness);
+  int bluVal = (b * brightness);
+
+  analogWrite(redPin, redVal);   // Write current values to LED pins
+  analogWrite(grnPin, grnVal);
+  analogWrite(bluPin, bluVal);
 }
