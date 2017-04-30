0c8b0649eca957e189cd8b0461b3479c630a635e
Josiah Pang <pang.josiah@gmail.com>
2017-04-29 17:06:59 -0700

    Clean up test sketches

---
+++ Misc Sketches/RGB_State_Change/RGB_State_Change.ino
@@ -0,0 +1,96 @@
+/*
+  State change detection (edge detection)
+
+ Often, you don't need to know the state of a digital input all the time,
+ but you just need to know when the input changes from one state to another.
+ For example, you want to know when a button goes from OFF to ON.  This is called
+ state change detection, or edge detection.
+
+ This example shows how to detect when a button or button changes from off to on
+ and on to off.
+
+ The circuit:
+ * pushbutton attached to pin 2 from +5V
+ * 10K resistor attached to pin 2 from ground
+ * LED attached from pin 13 to ground (or use the built-in LED on
+   most Arduino boards)
+
+ created  27 Sep 2005
+ modified 30 Aug 2011
+ by Tom Igoe
+
+This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/ButtonStateChange
+
+ */
+
+// ==========
+// Variables
+// ==========
+// this constant won't change:
+const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+const int ledPin = 21;       // the pin that the LED is attached to
+// Declares RGB LED pins
+int redPin = 5;   // Red LED,   connected to digital pin 5
+int grnPin = 6;  // Green LED, connected to digital pin 6
+int bluPin = 9;  // Blue LED,  connected to digital pin 9
+
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  // initialize the LED as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize serial communication:
+  pinMode(redPin, OUTPUT);   // sets the pins as output
+  pinMode(grnPin, OUTPUT);
+  pinMode(bluPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("----BEGIN----");
+}
+
+
+void loop() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      buttonPushCounter++;
+      Serial.println("on");
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonPushCounter);
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state,
+  //for next time through the loop
+  lastButtonState = buttonState;
+
+
+  // turns on the LED every four button pushes by
+  // checking the modulo of the button push counter.
+  // the modulo function gives you the remainder of
+  // the division of two numbers:
+  if (buttonPushCounter % 2 == 0) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+}
---
+++ Misc Sketches/rgb_color_input/rgb_color_input.ino
@@ -0,0 +1,47 @@
+int redPin = 5;   // Red LED,   connected to digital pin 9
+int grnPin = 6;  // Green LED, connected to digital pin 10
+int bluPin = 9;  // Blue LED,  connected to digital pin 11
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redPin, OUTPUT);   // sets the pins as output
+  pinMode(grnPin, OUTPUT);
+  pinMode(bluPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("----BEGIN----");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  if (Serial.available() > 0)
+  {
+    int r = Serial.readStringUntil(',').toInt();
+    int g = Serial.readStringUntil(',').toInt();
+    int b = Serial.readStringUntil(',').toInt();
+    Serial.print("R: ");
+    Serial.print(r);
+    Serial.print("; G: ");
+    Serial.print(g);
+    Serial.print("; B: ");
+    Serial.println(b);
+    ledColor(r, g, b);
+    // Do something awesome!
+  }
+}
+
+
+void ledColor(int r, int g, int b)
+{
+  int redVal = (r * 255) / 100;
+  int grnVal = (g * 255) / 100;
+  int bluVal = (b * 255) / 100;
+
+  analogWrite(redPin, redVal);   // Write current values to LED pins
+  analogWrite(grnPin, grnVal);
+  analogWrite(bluPin, bluVal);
+}
+
